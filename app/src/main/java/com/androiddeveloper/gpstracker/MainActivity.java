0255479f6a592404e1922276b3ee64deec65d7e6
makewheels <makewheels>
2020-09-02 22:20:25 +0800

    百度地图sdk

--- app/src/main/java/com/androiddeveloper/gpstracker/MainActivity.java
+++ app/src/main/java/com/androiddeveloper/gpstracker/MainActivity.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.baidu.mapapi.SDKInitializer;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SDKInitializer.initialize(this);
     }
 }
